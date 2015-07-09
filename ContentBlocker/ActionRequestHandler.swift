5df948999719e89089153279b8245c41dbd07a6b
Matt Garrison <mattsgarrison@gmail.com>
2015-07-09 10:19:46 -0600

    Adding an appropriate app icon, and adding a block list stolen from Ka-Block Safari Extension

---
+++ ContentBlocker/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  ContentBlocker
+//
+//  Created by Matthew Garrison on 7/9/15.
+//  Copyright © 2015 IconoclastLabs LLC. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
