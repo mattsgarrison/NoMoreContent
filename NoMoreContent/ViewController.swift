3be1b0a29e86ebafc2eb4ec821df7d821b05fb15
Matt Garrison <mattsgarrison@gmail.com>
2015-07-09 09:37:06 -0600

    Initial Commit

---
+++ NoMoreContent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoMoreContent
+//
+//  Created by Matthew Garrison on 7/9/15.
+//  Copyright © 2015 IconoclastLabs LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
